96f4835c7550ac6850e483d14691d8e3229f9f21
Plamen Stoilkov <sdplamen@gmail.com>
2024-07-16 20:00:45 +0300

    Update code.

--- amicable_numbers.py
+++ amicable_numbers.py
@@ -1,5 +1,3 @@
-print('Amicable pairs a and b are integers where the sum of the proper divisors of a is equal to the sum of the proper divisors of b and vice versa. The proper divisors of a are the positive integers that divide into a evenly (without leaving a remainder) but exclude the number itself.')
-
 def sum_proper_division(n):
     division_sum = 1
     limit = int(n ** 0.5) + 1
